a4689d28b594b8c015f5def92ed38369e248ab06
Milleand <140829447+Milleand@users.noreply.github.com>
2025-04-15 11:10:22 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import DataFetcher from "./DataFetcher.jsx";
+import FocusInput from "./FocusInput.jsx";
+
+export default function App() {
+  return (
+      <>
+        <DataFetcher/>
+        <FocusInput/>
+      </>
+  )
+
+}
+
